5c8337fe3de78a8e2893e301d14367cc8f33e9d2
Christopher J. Pacheco <christopherpacheco@storm.cis.fordham.edu>
2016-02-16 16:03:55 -0500

    HW 3

---
+++ Assignment3.cc
@@ -0,0 +1,21 @@
+// Assignment 3
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+
+	int x;
+	cout << "Input x = ";
+	cin >> x;
+	
+	int y;
+	cout << "Input y = ";
+	cin >> y;
+	
+	cout << "x+y = " <<
+	cout << "x-y = " << endl;
+	return 0;
+}
+
