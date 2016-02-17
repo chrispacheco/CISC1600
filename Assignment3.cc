8f07983b9f5b1255dc2ed8a6de0326813bfd605d
Christopher J. Pacheco <christopherpacheco@storm.cis.fordham.edu>
2016-02-16 21:39:40 -0500

    Correcting code

--- Assignment3.cc
+++ Assignment3.cc
@@ -6,6 +6,10 @@ using namespace std;
 int main ()
 {
 
+	string name;	
+	cout << "What's your name?";
+	cin >> name;
+
 	int a, b, c;
 	
 	cout << "Enter two numbers to add\n";
@@ -20,8 +24,10 @@ int main ()
 	cin >> x >> y;
 
 	z = x - y;
-	cout << "Sum of the entered numbers = " << z  << endl;
+	cout << "Difference of the entered numbers = " << z  << endl;
 	
+	cout << "Hi, " << name << ".";
+	cout << " I've calculated that " << a << "+" << b << "=" << c << ", " << "and " << x << "-" << y << "=" << z << endl;
 	return 0;
 
 }
